4c152e48170ff293e70983b943eedd7e1a070ba2
Will <wb226120@ohio.edu>
2022-01-18 11:27:52 -0500

    program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Will Buitendorp
+ *        Date: 1/18/22
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
